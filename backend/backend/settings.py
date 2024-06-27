9711d675792592d76b1cd1b2bc1eb27ba1b46b7c
kurealnum <cattledoger@gmail.com>
2024-06-27 08:55:02 -0400

    Progress made on setting up database, starting on docker-compose

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "rdi_database",
+        "USER": environ.get("DB_USERNAME"),
+        "PASSWORD": environ.get("DB_PASSWORD"),
+        "HOST": "0.0.0.0",
+        "PORT": environ.get("DB_PORT"),
     }
 }
 
